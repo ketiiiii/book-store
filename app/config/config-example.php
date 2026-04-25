657b20e7870338471efd0dc36814d061d47589d5
ketiiiii <dejankursevi@gmail.com>
2026-04-25 13:02:33 +0200

    Kreiran fajl za rad sa bazom

---
+++ app/config/config-example.php
@@ -0,0 +1,8 @@
+<?php
+const DB_HOST = 'localhost';
+const DB_USER = 'user';
+const DB_PASS = 'password';
+const DB_NAME = 'bookdb';
+const DB_PORT = 3306;
+const BASE_URL = 'http://localhost/book-store/public/';
+?>
